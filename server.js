ef813188be57926bfb48d121d76c4263f7aaf726
Holy Paul <brijeeta@gmail.com>
2021-11-14 21:51:39 -0500

    added mvc and basic folders

---
+++ server.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const routes = require('./controllers');
+// const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// express setup
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// handlebars setup
+const hbs = exphbs.create({});
+
+const sess = {
+    secret: '1234',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Express server listening on port ${PORT}!`));
+});
